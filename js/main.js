a8313e50da7b08f52235528fc9f76f891582247b
EdwardGoomba <ask@edwardvdesign.com>
2018-08-15 12:37:47 -0400

    Added toggle to menu

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@
 const offsiteToggle = document.querySelector('.jsOffsiteToggle')
 offsiteToggle.addEventListener('click', e => {
   console.log('Button clicked, push screen')
-  document.body.classList.add('offsite-is-open')
+  document.body.classList.toggle('offsite-is-open')
 })
