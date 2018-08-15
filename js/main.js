1e95d888f1146b7e6418114f80eab565b6198a4e
EdwardGoomba <ask@edwardvdesign.com>
2018-08-15 12:36:23 -0400

    Menu opens on icon click

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-// START EDITING YOUR JAVASCRIPT HERE
-// ===============
+
+const offsiteToggle = document.querySelector('.jsOffsiteToggle')
+offsiteToggle.addEventListener('click', e => {
+  console.log('Button clicked, push screen')
+  document.body.classList.add('offsite-is-open')
+})
